dff750f2117e28345e05036c44a68f9d8d23c742
easec <mats.johannesson@easec.se>
2018-08-13 18:07:09 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def main():
+    return 'Hej!! Min flaskapplikation, easec.'
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0')
+
